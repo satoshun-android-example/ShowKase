3082777313ba6c80b2c471251ea129fafbbba506
SatoShun <shun.sato1@gmail.com>
2021-03-12 00:56:05 +0000

    mainApp

--- mainApp/app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ mainApp/app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -1,14 +1,5 @@
 package com.github.satoshun.example
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.compose.setContent
 
-class AppActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    setContent {
-    }
-  }
-}
+class AppActivity : AppCompatActivity(R.layout.app_act)
