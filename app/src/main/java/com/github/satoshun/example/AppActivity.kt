860004099e2a6ed574a0c530b2cbc7d5b23b2382
SatoShun <shun.sato1@gmail.com>
2021-03-13 10:13:45 +0000

    lib

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -3,6 +3,9 @@ package com.github.satoshun.example
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class AppActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,4 +14,10 @@ class AppActivity : AppCompatActivity() {
     setContent {
     }
   }
+}
+
+@Preview
+@Composable
+fun PreviewSample() {
+  Text("test")
 }
---
+++ lib/src/main/java/com/github/satoshun/example/sample/SampleFragment.kt
@@ -0,0 +1,12 @@
+package com.github.satoshun.example.sample
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.github.satoshun.example.lib.R
+
+class SampleFragment : Fragment(R.layout.sample_frag) {
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+  }
+}
